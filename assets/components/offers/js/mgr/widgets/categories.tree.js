b29a417ba76c2e6c14063c726a92176307b61452
Aspirin4ik <91979979+Aspirin4ik@users.noreply.github.com>
2026-03-07 15:27:47 +0700

    Update categories.tree.js

--- assets/components/offers/js/mgr/widgets/categories.tree.js
+++ assets/components/offers/js/mgr/widgets/categories.tree.js
@@ -2,18 +2,34 @@ Offers.tree.Categories = function(config) {
     config = config || {};
 
     Ext.applyIf(config, {
+        xtype: 'treepanel',
+        id: 'offers-tree-categories',
         url: Offers.config.connectorUrl,
-        action: 'mgr/product/getcategories',
-        root_id: 8, // ID корневой категории
+
+        // ВАЖНО: только baseParams работает в MODx.tree.Tree
+        baseParams: {
+            action: 'mgr/product/getcategories'
+        },
+
+        root_id: 8,
+        root_name: 'Каталог',
         rootVisible: true,
+
         useArrows: true,
         autoScroll: true,
         border: false,
+
         listeners: {
             click: function(node) {
                 var grid = Ext.getCmp('offers-grid-products');
-                grid.getStore().baseParams.category_id = node.id;
-                grid.getStore().load();
+                if (grid) {
+                    grid.getStore().load({
+                        params: {
+                            action: 'mgr/product/getlist',
+                            category_id: node.id
+                        }
+                    });
+                }
             }
         }
     });

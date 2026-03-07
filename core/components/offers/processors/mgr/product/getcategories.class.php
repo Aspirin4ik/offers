7c4ff7fb5bf283298f4d968269571c37db61e2e1
Aspirin4ik <91979979+Aspirin4ik@users.noreply.github.com>
2026-03-07 15:15:10 +0700

    Update getcategories.class.php

--- core/components/offers/processors/mgr/product/getcategories.class.php
+++ core/components/offers/processors/mgr/product/getcategories.class.php
@@ -3,25 +3,38 @@
 class ProductGetCategoriesProcessor extends modProcessor {
 
     public function process() {
-        $parent = (int)$this->getProperty('id', 8);
+        // Узел, который раскрываем
+        $parent = (int)$this->getProperty('node', 8);
 
+        // Получаем только категории miniShop2
         $children = $this->modx->getCollection('modResource', [
             'parent' => $parent,
+            'deleted' => 0,
+            'published' => 1,
             'class_key' => 'msCategory'
         ]);
 
         $list = [];
 
         foreach ($children as $child) {
+
+            // Проверяем, есть ли у категории подкатегории
+            $hasChildren = $this->modx->getCount('modResource', [
+                'parent' => $child->get('id'),
+                'deleted' => 0,
+                'published' => 1,
+                'class_key' => 'msCategory'
+            ]) > 0;
+
             $list[] = [
                 'text' => $child->get('pagetitle'),
                 'id' => $child->get('id'),
-                'leaf' => false,
+                'leaf' => !$hasChildren, // если нет подкатегорий — лист
                 'cls' => 'folder'
             ];
         }
 
-        return $this->modx->toJSON($list);
+        return $this->outputArray($list);
     }
 }
 

d353be7f629e1b45618624f52dd4d7c71f433c51
Aspirin4ik <91979979+Aspirin4ik@users.noreply.github.com>
2026-03-09 19:12:55 +0700

    Delete core/components/offers directory

--- core/components/offers/processors/mgr/offeritem/remove.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class OfferItemRemoveProcessor extends modObjectRemoveProcessor {
-    public $classKey = 'OfferItem';
-    public $objectType = 'OfferItem';
-}
-
-return 'OfferItemRemoveProcessor';
